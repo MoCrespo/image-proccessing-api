64fef1b3ac72112a0d8a2d267a6a6d30f4059d96
MoCrespo <mcrespo495@gmail.com>
2022-11-25 20:01:55 +0200

    make varaibles typed and modify some conditions

--- src/routes/image.routes.ts
+++ src/routes/image.routes.ts
@@ -7,8 +7,8 @@ const router = Router();
 
 router.get('/api/images', async (req, res) => {
   const filename: string = req.query.filename as string;
-  const width = req.query.width;
-  const height = req.query.height;
+  const width: string = req.query.width as string;
+  const height: string = req.query.height as string;
 
   const imageFile: string = path.join(
     __dirname,
@@ -28,13 +28,11 @@ router.get('/api/images', async (req, res) => {
     }
   } else if (filename && (!width || !height)) {
     try {
-      if (typeof width === 'undefined' && typeof height === 'undefined')
+      if (typeof width === 'undefined' && typeof height === 'undefined') {
         return res.status(200).sendFile(imageFile);
-      if (typeof width === 'undefined') throw 'width undefined';
-      if (typeof height === 'undefined') throw 'height undefined';
-
-      if (typeof width !== 'number' || typeof height !== 'number') {
-        throw 'width and height should be a number';
+      }
+      if (width.length == 0 || height.length == 0) {
+        throw 'width or height undefined';
       }
     } catch (err) {
       return res.status(400).send({ error: err });
@@ -45,12 +43,15 @@ router.get('/api/images', async (req, res) => {
         return res.sendFile(imageThumb);
       }
       if (!fs.existsSync(imageThumb)) {
+        console.log(width, height);
+        console.log(typeof width, typeof height);
+
         try {
           sharp(imageFile)
-            .resize(parseInt(width as string), parseInt(height as string))
+            .resize(parseInt(width), parseInt(height))
             .toFile(imageThumb, (err) => {
               if (err) throw err;
-              res.status(200).sendFile(imageThumb);
+              return res.status(200).sendFile(imageThumb);
             });
         } catch (err) {
           return res.status(400).send({ error: err });
